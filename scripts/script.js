d4dc08bfed23fc3c4d917a838bb8b26bbc98ebfc
Nigel Teo <e0694450@u.nus.edu>
2024-04-27 21:34:44 +0800

    Update index.html

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,14 @@
 // script.js
 
-document.addEventListener('keydown', function(event) {
-    if (event.key === 'x') {
-        document.body.classList.toggle('dark-mode');
-    }
+document.addEventListener("keydown", function (event) {
+  if (event.key === "x") {
+    document.body.classList.toggle("dark-mode");
+  }
 });
-  
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
